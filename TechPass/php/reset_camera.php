8d749b7d5db8b4cf222caccbbb5f7053f0994e73
SM03KY <smoeky0831@gmail.com>
2026-05-09 13:19:40 +0800

    Delete TechPass directory

--- TechPass/php/reset_camera.php
+++
@@ -1,8 +0,0 @@
-<?php
-// This deletes the old status note so the system is forced to wait for a brand new photo
-$file = 'status.txt';
-if (file_exists($file)) {
-    unlink($file);
-}
-echo "Ready for new capture";
-?>
